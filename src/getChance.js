897dd4e0dc6cc7894fa5d1bb429c0cd6310c2e56
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-11-17 22:50:14 +0100

    Setting up environment

---
+++ src/getChance.js
@@ -0,0 +1,5 @@
+function getChance(n, x, a) {
+  return 'getChance';
+}
+
+module.exports = getChance;
---
+++ src/getChance.test.js
@@ -0,0 +1,7 @@
+const getChance = require('./getChance');
+
+describe('getChance', () => {
+  it('should return getChance', () => {
+    expect(getChance()).toBe('getChance');
+  });
+});
